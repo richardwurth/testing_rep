e3e2862b9ad8cd23b2ce8a6373fc695c4656a981
Richard Wurth <richard.wurth91@gmail.com>
2017-01-03 20:57:47 -0700

    What happens when I'm logged out of GitHub and I try to push

--- anotherJavaScript.js
+++ anotherJavaScript.js
@@ -2,5 +2,4 @@ var hello_with_accent = "Hello, ya'll"; /*On second thought, nevermind, you're f
 var manager = "Can't make up his mind";
 var hello = "Hello, ya'll"; /*On second thought, nevermind, you're fine. But... then again...*/
 
-console.log(hello);
- 
+console.log(hello_with_accent);
