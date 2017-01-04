92d7bd7dc3872025c6c0c520cd72c2ed5d87977c
richardwurth <richard.wurth91@gmail.com>
2017-01-03 20:52:13 -0700

    Update anotherJavaScript.js

--- anotherJavaScript.js
+++ anotherJavaScript.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-var hello = "Hello, fellow humans"; /*Fixed*/
-=======
-var hello = "Hello, ya'll"; /*On second thought, nevermind, you're fine.*/
->>>>>>> fe09065aa22df2dea8e64981152f636c359fc6de
+var hello = "Hello, ya'll"; /*On second thought, nevermind, you're fine. But... then again...*/
 
 console.log(hello);
