0b19337104b775a9543f713c484c3cc5b4bbe791
Richard Wurth <richard.wurth91@gmail.com>
2017-01-03 20:53:06 -0700

    To test conflicting changes

--- anotherJavaScript.js
+++ anotherJavaScript.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-var hello = "Hello, fellow humans"; /*Fixed*/
-=======
+
 var hello = "Hello, ya'll"; /*On second thought, nevermind, you're fine.*/
->>>>>>> fe09065aa22df2dea8e64981152f636c359fc6de
+var manager = "Can't make up his mind";
 
 console.log(hello);
