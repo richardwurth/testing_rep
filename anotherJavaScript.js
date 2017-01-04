7d4da23a1d9533e7273a27a93d3330e845aaa6cc
Richard Wurth <rwurth@communitect.com>
2017-01-04 08:06:56 -0700

    Uploading from a different computer, added console log

--- anotherJavaScript.js
+++ anotherJavaScript.js
@@ -1,5 +1,9 @@
-var hello_with_accent = "Hello, ya'll"; /*On second thought, nevermind, you're fine.*/
+var hello_with_accent = "Hello, ya'll"; /*On second thought,
+nevermind, you're fine.*/
 var manager = "Can't make up his mind";
-var hello = "Hello, ya'll"; /*On second thought, nevermind, you're fine. But... then again...*/
+var hello = "Hello, ya'll"; /*On second thought, nevermind, you're fine.
+ But... then again...*/
 
 console.log(hello_with_accent);
+
+console.log("Testing to connect git hub to another computer");
